7837b4d12d22772f1358a9d240e9c3907949f1c9
Tilak Kandel <tilakkndl2002@gmail.com>
2023-07-11 01:59:50 -0400

    First day

---
+++ day-1/project/name-generator.py
@@ -0,0 +1,5 @@
+print("Welcome to the first Name Generating Project\n")
+city = input("Enter your city name?\n")
+pet_name = input("Enter your pet name?\n")
+print("Your name is "+ pet_name + " " + city)
+
---
+++ day-1/simpleProgram.py
@@ -0,0 +1,8 @@
+print("Hello Python community")
+
+# name = input("Enter your name:")
+# print("YOur name is " + name)
+
+#excersise 3
+name = input("Enter your name: ")
+print("Your name's length is "+ str(len(name)))
