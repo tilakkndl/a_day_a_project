de6f3ea8dc6f7f338077fa3a69c826759c913bc0
Tilak Kandel <tilakkndl2002@gmail.com>
2023-07-13 23:51:21 -0400

    Day 4 complete

---
+++ day4/learning_randomization_list.py
@@ -0,0 +1,10 @@
+import random
+
+# print(random.randint(5, 8))
+# print(random.random())
+# #random between 0 and 5
+# print(random.random()+float(random.randint(0, 4)))
+
+#lists 
+fruits = ["apple", "orange", "grapes"]
+print(fruits[2])
